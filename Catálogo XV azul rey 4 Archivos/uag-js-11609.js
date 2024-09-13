c2c0d8941d592ccc1b4812f7e93c073c528d0568
willy8265 <lopwilly8265@gmail.com>
2024-09-12 19:50:44 -0600

    Add files via upload

---
+++ Catálogo XV azul rey 4 Archivos/uag-js-11609.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBCountdown.init( '.uagb-block-75d81278', {"block_id":"75d81278","endDateTime":"2024-05-31T01:45:00.000Z","showDays":true,"showHours":true,"showMinutes":true,"isFrontend":true,"timerEndAction":"zero","redirectURL":""} );
+});
+				window.addEventListener( 'DOMContentLoaded', () => {
+					const blockScope = document.querySelector( '.uagb-block-cc1a6671' );
+					if ( ! blockScope ) {
+						return;
+					}
+
+					const anchorElement = blockScope.querySelector('a');
+					if (!anchorElement) {
+						return;
+					} 
+
+					 
+					blockScope.addEventListener('keydown', (event) => {
+						if ( 13 === event.keyCode || 32 === event.keyCode ) {
+							event.preventDefault();
+							 
+							anchorElement.click();	
+						}
+					} );
+				} );
+			window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-5e187dae' );
+});
+				window.addEventListener( 'DOMContentLoaded', () => {
+					const blockScope = document.querySelector( '.uagb-block-c1b4ea56' );
+					if ( ! blockScope ) {
+						return;
+					}
+
+					const anchorElement = blockScope.querySelector('a');
+					if (!anchorElement) {
+						return;
+					} 
+
+					 
+					blockScope.addEventListener('keydown', (event) => {
+						if ( 13 === event.keyCode || 32 === event.keyCode ) {
+							event.preventDefault();
+							 
+							anchorElement.click();	
+						}
+					} );
+				} );
+			window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-d1bc24e0' );
+});
+window.addEventListener("DOMContentLoaded", function(){
+	UAGBTimelineClasses( {"block_id":"e58b2e01","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"center"}, '.uagb-block-e58b2e01' );
+});
+window.addEventListener("resize", function(){
+	UAGBTimelineClasses( {"block_id":"e58b2e01","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"center"}, '.uagb-block-e58b2e01' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-625a6038' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-8526cd7d' );
+});
+ });
